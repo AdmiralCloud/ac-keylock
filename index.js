91f253bb28013e29c559551ca7e346049a2e64f6
Mark Poepping <mark.poepping@mmpro.de>
2021-05-01 08:33:41 +0200

    fix(App): Use global var instead of this | MP
    Use global var instead of this

--- index.js
+++ index.js
@@ -3,31 +3,36 @@ const { v4: uuidV4 } = require('uuid')
 
 const NodeCache = require( "node-cache" )
 
-const keyLock = function() {
+const keyLock = () => {
+
+  let lockBaseParams = {} 
+ 
   /**
    * @param params.redis {Instance} Redis instance to use
    * @param params.logger {Instance} optional logger (e.g. Winston). Falls back to console
    * @param params.logLevel {String} optional logLevel
    */
 
-  const init = async function(params) {
-    this.redis = params.redis
-    this.logger = _.get(params, 'logger', console)
-    this.logLevel = _.get(params, 'logLevel', 'log')
+  const init = async (params) => {
+    lockBaseParams = {
+      redis: _.get(params, 'redis'),
+      logger: _.get(params, 'logger', console),
+      logLevel: _.get(params, 'logLevel', 'log')
+    }
 
     // make a test connection
-    if (this.redis) {
+    if (lockBaseParams.redis) {
       const testKey = uuidV4()
       try {
-        await this.redis.set(testKey, 1, 'EX', 1)
+        await lockBaseParams.redis.set(testKey, 1, 'EX', 1)
       }
       catch(e) {
-        this.logger['error']({ message: 'cannotConnectToRedis' })
+        lockBaseParams.logger['error']({ message: 'cannotConnectToRedis' })
       }
     }
     else {
       // use memory cache
-      this.storage = new NodeCache()
+      lockBaseParams.storage = new NodeCache()
     }
   }
 
@@ -36,7 +41,7 @@ const keyLock = function() {
    * @param params.expires {Integer} optional seconds to expire the key automatically
    * @param cb (err or null) err can be 423 (redis key is locked) or a real error or null
    */
-  const lockKey = async function(params) {
+  const lockKey = async (params) => {
     const key = params.key
     if (!key) {
       throw Error('ackeylock_lockKey_key_isRequired')
@@ -44,45 +49,45 @@ const keyLock = function() {
     const expires = (params.expires && parseInt(params.expires)) || 10 // 10 seconds default value
     const value = params.value || uuidV4()
 
-    if (this.redis) {
-      let result = await this.redis.set(key, value, 'EX', expires, 'NX')
+    if (lockBaseParams.redis) {
+      let result = await lockBaseParams.redis.set(key, value, 'EX', expires, 'NX')
       if (result === 'OK') return { value }
       else return { state: 423 }
     }
     else {
-      if (this.storage.has(key)) return { state: 423 }
-      this.storage.set(key, value, expires)
+      if (lockBaseParams.storage.has(key)) return { state: 423 }
+      lockBaseParams.storage.set(key, value, expires)
       return { value }
     }
   }
 
-  const releaseLock = async function(params) {
+  const releaseLock = async (params) => {
     const key = params.key
     if (!key) {
       throw Error('ackeylock_releaseLock_key_isRequired')
     }
     const value = params.value
 
-    if (this.redis) {
+    if (lockBaseParams.redis) {
       if (value) {
-        let result = await this.redis.get(key)
+        let result = await lockBaseParams.redis.get(key)
         if (result !== value) {
-          this.logger['error']('ackeylock_releaseLock_valueMismatch | %j', { key, expected: result, value })
+          lockBaseParams.logger['error']('ackeylock_releaseLock_valueMismatch | %j', { key, expected: result, value })
           throw Error('ackeylock_releaseLock_valueMismatch')
         }  
       }
-      await this.redis.del(key)
+      await lockBaseParams.redis.del(key)
       return { state: 200 }
     }
     else {
       if (value) {
-        let result = this.storage.get(key)
+        let result = lockBaseParams.storage.get(key)
         if (result !== value) {
-          this.logger['error']('ackeylock_releaseLock_valueMismatch | %j', { key, expected: result, value })
+          lockBaseParams.logger['error']('ackeylock_releaseLock_valueMismatch | %j', { key, expected: result, value })
           throw Error('ackeylock_releaseLock_valueMismatch')  
         }
       }
-      this.storage.del(key)
+      lockBaseParams.storage.del(key)
       return { state: 200 }
     }
   }
